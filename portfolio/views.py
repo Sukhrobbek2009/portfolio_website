69c5185a41e922dce12d2b4216c4646cba04c26b
Sam <sukhrobbekrustamov26@gmail.com>
2026-02-22 09:54:30 -0800

    add urls

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project
 
-# Create your views here.
+
+def projects(request):
+    projects = Project.objects.all()
+    return render(request, 'project.html', {'projects':projects})
+
+
+
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, 'project_detail.html', {'project': project})
+
+def profile(request):
+    return render(request, 'profile.html')
